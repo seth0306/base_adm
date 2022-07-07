733d41bce284b0f73eec9ba0140e84adf8e20fe9
Motoaki Kato <seth0306@gmail.com>
2022-07-07 15:06:18 +0900

    deleted unused files

--- lib/components/provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-/// ユーザープロバイダー
-final userProvider = StateProvider((ref) {
-  return FirebaseAuth.instance.currentUser;
-});
